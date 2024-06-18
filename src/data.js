317be7708c4afc49887faf69f66fe1a8104a7125
sumitnagrikar <sumitnagrikar1@gmail.com>
2024-06-19 00:02:12 +0530

    bookmyshow backend redy to deploy

---
+++ src/data.js
@@ -0,0 +1,15 @@
+let moviesList = [
+    "Suraj par mangal bhari",
+    "Tenet",
+    "The war with grandpa",
+    "The Witches",
+    "The Craft: Legacy",
+    "The Informer",
+    "Gadvach lagin",
+    "The SpongeBob Movie: Sponge on the Run",
+  ];
+  let slots = ["10:00 AM", "01:00 PM", "03:00 PM", "08:00 PM"];
+  let seats = ["A1", "A2", "A3", "A4", "D1", "D2"];
+  exports.moviesList = moviesList;
+  exports.slots = slots;
+  exports.seats = seats;
