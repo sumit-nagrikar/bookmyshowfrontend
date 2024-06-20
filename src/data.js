103c4ea2a8771e1e72f17692aaad537d128dad5e
sumitnagrikar <sumitnagrikar1@gmail.com>
2024-06-20 15:47:47 +0530

    updated some movie data

--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
+//This data I am using to send a post req on backend server for book a ticket
+
 let moviesList = [
     "Suraj par mangal bhari",
     "Tenet",
@@ -5,11 +7,16 @@ let moviesList = [
     "The Witches",
     "The Craft: Legacy",
     "The Informer",
-    "Gadvach lagin",
     "The SpongeBob Movie: Sponge on the Run",
+    "The Croods: A New Age",
+    "The Comeback Trail",
+    "The Night House",
   ];
+
   let slots = ["10:00 AM", "01:00 PM", "03:00 PM", "08:00 PM"];
+
   let seats = ["A1", "A2", "A3", "A4", "D1", "D2"];
+
   exports.moviesList = moviesList;
   exports.slots = slots;
   exports.seats = seats;
